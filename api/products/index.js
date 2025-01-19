feae7a74702ffb29b77c0e7d458739a3abd0c8c4
Denzil Serrao <115720007+DenzilSerrao@users.noreply.github.com>
2025-01-19 15:14:50 +0530

    removed local CORS from product.js

--- api/products/index.js
+++ api/products/index.js
@@ -3,17 +3,7 @@ import { Product } from '../../src/models/product.js';
 import { auth } from '../../middleware/auth.js';
 
 export default async function handler(req, res) {
-  // CORS Headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://beauty-shop-frontend-l8yf.vercel.app'); // Your frontend domain
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // If you want to allow cookies or credentials
-
-  // Handle preflight request
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end(); // Send a 200 response for OPTIONS requests
-  }
-
+  
   try {
     await connectDB();
 
