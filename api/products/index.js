6bc85c8bda12848254ffdb1299c2f6363a33c555
Denzil Serrao <115720007+DenzilSerrao@users.noreply.github.com>
2025-01-19 10:05:12 +0530

    added log outputs and cors headers

--- api/products/index.js
+++ api/products/index.js
@@ -3,12 +3,24 @@ import { Product } from '../../src/models/product.js';
 import { auth } from '../../middleware/auth.js';
 
 export default async function handler(req, res) {
+  // CORS Headers
+  res.setHeader('Access-Control-Allow-Origin', 'https://beauty-shop-frontend-l8yf.vercel.app'); // Your frontend domain
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true'); // If you want to allow cookies or credentials
+
+  // Handle preflight request
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end(); // Send a 200 response for OPTIONS requests
+  }
+
   try {
     await connectDB();
 
     switch (req.method) {
       case 'GET':
         const products = await Product.find({ isActive: true });
+        console.log('products:', products);
         return res.status(200).json({ products });
 
       case 'POST':
