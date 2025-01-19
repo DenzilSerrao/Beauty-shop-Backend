7c13e8de783c69630acdd732a8710d2635137b50
Denzil Serrao <115720007+DenzilSerrao@users.noreply.github.com>
2025-01-19 09:42:48 +0530

    Updated model imports

--- api/auth/register.js
+++ api/auth/register.js
@@ -1,5 +1,5 @@
 import { connectDB } from '../../lib/db.js';
-import { User } from '../../models/user.js';
+import { User } from '../../src/models/user.js';
 import jwt from 'jsonwebtoken.js';
 
 export default async function handler(req, res) {
--- api/orders/index.js
+++ api/orders/index.js
@@ -1,5 +1,5 @@
 import { connectDB } from '../../lib/db.js';
-import { Order } from '../../models/order.js';
+import { Order } from '../../src/models/order.js';
 import { auth } from '../../middleware/auth.js';
 
 export default async function handler(req, res) {
