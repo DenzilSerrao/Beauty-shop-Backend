1e81883776827aa3293391f0d3ecf3a3cc408388
Denzil Serrao <115720007+DenzilSerrao@users.noreply.github.com>
2025-01-20 17:51:48 +0530

    removed duplicate connectDB

--- api/orders/index.js
+++ api/orders/index.js
@@ -8,7 +8,6 @@ export default async function handler(req, res) {
   }
 
   if (req.method === 'GET') {
-    await connectDB();
     try {
       const orders = await getOrders(req, res);
       res.status(200).json(orders);
