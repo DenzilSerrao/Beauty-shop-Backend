519dec0f0bfdb44275fd1890fd58804052cda141
Denzil Serrao <115720007+DenzilSerrao@users.noreply.github.com>
2025-01-20 17:33:14 +0530

    modified db params and connectDB for orders

--- api/orders/index.js
+++ api/orders/index.js
@@ -8,6 +8,7 @@ export default async function handler(req, res) {
   }
 
   if (req.method === 'GET') {
+    await connectDB();
     try {
       const orders = await getOrders(req, res);
       res.status(200).json(orders);
