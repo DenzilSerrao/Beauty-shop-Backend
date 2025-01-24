f4f12a8d7e19f9bf9bf74997667f46b9cd57a6dd
Denzil Serrao <115720007+DenzilSerrao@users.noreply.github.com>
2025-01-24 15:07:18 +0530

    updated import

--- controllers/orders.js
+++ controllers/orders.js
@@ -3,7 +3,7 @@ import { Order } from '../models/order.js';
 import { User } from '../models/user.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { NotFoundError, ValidationError } from '../utils/errors.js';
-import generateInvoice from '../utils/invoice.js';
+import { generateInvoice } from '../utils/invoice.js';
 import mongoose from 'mongoose';
 
 export const getOrders = asyncHandler(async (req, res) => {
